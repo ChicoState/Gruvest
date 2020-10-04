5f8f64aa045c3575d82e5a30cb7b03ffce49f67c
blueweaver <brandonweaver2011@hotmail.com>
2020-10-04 12:29:12 -0700

    adds function to upvote and downvote buttons

--- Gruvest-Django/mysite/myapp/models.py
+++ Gruvest-Django/mysite/myapp/models.py
@@ -1,20 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class PostModel(models.Model):
     header = models.CharField(max_length = 100)
     post = models.CharField(max_length = 240)
-    UpVote = models.IntegerField(default = 1)
-    DownVote = models.IntegerField(default = 1)
+    UpVote = models.ManyToManyField(User, related_name='pitchUp')
+    DownVote = models.ManyToManyField(User, related_name='pitchDown')
 
     def __str__(self):
         return self.post
+    
+    def GetUpVotes(self):
+        return self.UpVote.count()
 
     def GetTotalVotes(self):
-        return self.UpVote - self.DownVote
+        return self.UpVote.count() - self.DownVote.count()
     
     def AddUpVote(self):
         self.UpVote += 1
         return self.UpVote
 
-   
+    def AddDownVote(self):
+        self.DownVote += 1
+        return self.DownVote
+    
+
--- Gruvest-Django/mysite/myapp/views.py
+++ Gruvest-Django/mysite/myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from . import forms
 from . import models
+
 
 # Create your views here.
 def index(request):
@@ -13,15 +15,22 @@ def index(request):
 
     else:
         post_form = forms.postForm()
-    
     title = "Gruvest"
     posts = models.PostModel.objects.all()
-   
     context = {
         "post":posts,
         "title":title,
         "form":post_form,
-
     }
     return render(request, "base.html", context = context)
 
+def UpVoteView(request, pk):
+    post = get_object_or_404(models.PostModel, id=request.POST.get('post_id'))
+    post.UpVote.add(request.user)
+    return HttpResponseRedirect(reverse("main"))
+    
+def DownVoteView(request, pk):
+    post = get_object_or_404(models.PostModel, id=request.POST.get('post_id'))
+    post.DownVote.add(request.user)
+    return HttpResponseRedirect(reverse("main"))
+    
