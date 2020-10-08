0fd2bba782ce731e0e7ed2b865359c5fe0a2d60f
jhowell1 <jhowell1@mail.csuchico.edu>
2020-10-07 20:52:27 -0700

    Adds beginning of class-based views.

--- Gruvest-Django/mysite/myapp/urls.py
+++ Gruvest-Django/mysite/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'main'),
+    path('', views.IndexView.as_view(), name= 'main'),
     path('like/<int:pk>', views.upVoteView, name='upVotePost'),
     path('dislike/<int:pk>', views.downVoteView, name='downVotePost'),
     #path('comment/<int:pk>', views.commentView, name='commentPost')
--- Gruvest-Django/mysite/myapp/views.py
+++ Gruvest-Django/mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import forms
 from . import models
 
@@ -22,19 +23,19 @@ def index(request):
             "title":title,
             "form":post_form,
         }
-    return render(request, "base.html", context = context)
+    return render(request, "base.html", context=context)
 
 # Creates view for upvoting
 def upVoteView(request, pk):
     post = get_object_or_404(models.PostModel, id=request.POST.get('post_id'))
-    post.upVotes = post.upVotes + int(1)
+    post.upVotes += int(1)
     post.save()
     return HttpResponseRedirect(reverse("main"))
 
 # Creates view for downvoting
 def downVoteView(request, pk):
     post = get_object_or_404(models.PostModel, id=request.POST.get('post_id'))
-    post.downVotes = post.downVotes + int(1)
+    post.downVotes += int(1)
     post.save()
     return HttpResponseRedirect(reverse("main"))
 
