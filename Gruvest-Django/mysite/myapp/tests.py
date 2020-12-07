373f67d960348e623915bb346c884ec4fca2ea9a
blueweaver <brandonweaver2011@hotmail.com>
2020-12-06 22:25:57 -0800

    add travis CI and test files

--- Gruvest-Django/mysite/myapp/tests.py
+++ Gruvest-Django/mysite/myapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from . import models
 # Create your tests here.
+
+class PitchTestCase(TestCase):
+    def setUp(self):
+        models.PostModel.objects.create(header="test", post="hi", cost=20, upVotes=20, downVotes=2)
+       
+    def test_pitch_creation(self):
+        testPitch =  models.PostModel.objects.get(header="test")
+        self.assertEqual(testPitch.getTotalVotes(), 18)
